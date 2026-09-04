fa9737046d2f5fed6f4d6d20b9fe246a09a9117e
diegoz-netizen <diego.panez@tecsup.edu.pe>
2026-09-03 19:39:00 -0500

    Modificacion de la clase StudentController

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,13 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+    public double calcular(double a, double b, String operacion) {
+        return switch (operacion.toLowerCase()) {
+            case "suma", "+" -> a + b;
+            case "resta", "-" -> a - b;
+            case "multiplica", "*" -> a * b;
+            case "divide", "/" -> b != 0 ? a / b : 0;
+            default -> 0;
+        };
+    }
 }
